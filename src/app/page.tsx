17b0374777b9efc34f67b4180eda21a173e1aa8c
sydney-choi <soyeong0730@gmail.com>
2024-08-05 22:16:01 +0900

    chore: 불필요한 스타일 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,7 @@ import { Collapse } from '@/components/common/collapse';
 
 const Page = () => (
   <Flex>
-    <Collapse
-      style={{
-        position: 'relative',
-        width: '20rem',
-        minWidth: '384px',
-        height: '100%',
-        boxShadow: '0 -6px 10px rgb(0 0 0 / 30%)',
-        zIndex: '999',
-        transition: '0.2s',
-      }}
-    >
+    <Collapse>
       <Main />
     </Collapse>
     <Box flex="1">
